1f8fad690b90c27c69d3707dcfde389035c98b8b
ThabangManaka <thabangdansteyn@gmail.com>
2020-09-22 15:21:28 +0200

    Alert Box

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { TaskService } from '../task.service';
 
 @Component({
   selector: 'app-task',
@@ -7,9 +9,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _taskService: TaskService,
+  public alertController: AlertController) { }
 
   ngOnInit() {
   }
+  get AllTaskList() {
+    return this._taskService.getAllTask();
+  }
 
+  async  addNewTask() {
+    const alert = await this.alertController.create({
+      header: 'Add New Task',
+        inputs: [
+        {
+          name: 'content',
+          type: 'text',
+          placeholder: 'Content'
+        },
+    
+        {
+          name: 'status',
+          type: 'text',
+          placeholder: 'Status'
+        },
+            {
+          name: 'priority',
+          type: 'text',
+          placeholder: 'Priority'
+        },
+        ],
+      buttons: [
+        { text: "Submit", handler: (res) =>{
+          this._taskService.addTask(res);
+
+        }}
+      
+      ],
+    });
+
+    await alert.present();
+    let result = await alert.onDidDismiss();
+    console.log(result);
+  }
 }
